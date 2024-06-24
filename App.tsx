0cb5eb31cb05253b7a2b68bee49888c99ce33d42
Francis Dias <francisdiasbr@gmail.com>
2024-06-24 20:09:06 -0300

    Configure ESLint

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -15,9 +16,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 });
