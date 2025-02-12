b79ade9cfd203192e19d3e97f9766fcbcccfef6a
francisdiasbr <francisdiasbr@gmail.com>
2025-02-12 19:59:02 -0300

    Legenda da imagem

--- lib/features/uploadImages/uploadImagesSlice.ts
+++ lib/features/uploadImages/uploadImagesSlice.ts
@@ -4,32 +4,38 @@ import baseService from '../../api/service';
 
 interface UploadImageState {
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  uploadStatus: 'idle' | 'loading' | 'succeeded' | 'failed';
   error: string | null;
   objectName: string | null;
   imageUrl: string | null;
   imageUrls: string[];
   imageNames: string[];
+  imageSubtitles: string[];
   imageCache: {
     [tconst: string]: {
       urls: string[];
       names: string[];
     }
   }
+  subtitleStatus: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
 const initialState: UploadImageState = {
   status: 'idle',
+  uploadStatus: 'idle',
   error: null,
   objectName: null,
   imageUrl: null,
   imageUrls: [],
   imageNames: [],
+  imageSubtitles: [],
   imageCache: {},
+  subtitleStatus: 'idle',
 };
 
 
 export const fetchAllImageUrls = createAsyncThunk<
-  { urls: string[]; names: string[] },
+  { urls: string[]; names: string[]; subtitles: string[] },
   { tconst: string }
 >(
   'uploadImages/fetchAllImageUrls',
@@ -37,17 +43,11 @@ export const fetchAllImageUrls = createAsyncThunk<
     try {
       const response = await baseService.get(`images/${tconst}`);
       
-      if (response.url) {
-        return {
-          urls: [response.url],
-          names: [`image_${tconst}`],
-        };
-      }
-
       if (response.images) {
         return {
           urls: response.images.map((image: { url: string }) => image.url),
-          names: response.images.map((image: { filename: string }) => image.filename)
+          names: response.images.map((image: { filename: string }) => image.filename),
+          subtitles: response.images.map((image: { subtitle: string }) => image.subtitle)
         };
       }
 
@@ -89,27 +89,43 @@ export const uploadMovieImage = createAsyncThunk<
   }
 );
 
-// export const deleteImage = createAsyncThunk<
-//   void,
-//   { tconst: string; filename: string }
-// >(
-//   'uploadImages/deleteImage',
-//   async ({ tconst, filename }, { rejectWithValue }) => {
-//     try {
-//       await baseService.delete(
-//         `images/delete-image/${tconst}/${filename}`
-//       );
-//       console.log(`Imagem ${filename} deletada com sucesso.`);
-//     } catch (error) {
-//       console.error('Erro ao deletar imagem:', error);
-//       if (error instanceof Error) {
-//         return rejectWithValue(error.message);
-//       } else {
-//         return rejectWithValue('An unexpected error occurred');
-//       }
-//     }
-//   }
-// );
+export const deleteImage = createAsyncThunk<
+  void,
+  { tconst: string; filename: string }
+>(
+  'uploadImages/deleteImage',
+  async ({ tconst, filename }, { rejectWithValue }) => {
+    try {
+      await baseService.delete(
+        `images/${tconst}/${filename}`
+      );
+      console.log(`Imagem ${filename} deletada com sucesso.`);
+    } catch (error) {
+      console.error('Erro ao deletar imagem:', error);
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      } else {
+        return rejectWithValue('An unexpected error occurred');
+      }
+    }
+  }
+);
+
+export const updateImageSubtitle = createAsyncThunk(
+  'uploadImages/updateSubtitle',
+  async ({ tconst, filename, subtitle }: { tconst: string, filename: string, subtitle: string }) => {
+    const body = {
+      subtitle: subtitle
+    }
+    const response = await baseService.put(`images/${tconst}/${filename}`, body as any);
+    
+    if (!response.ok) {
+      throw new Error('Falha ao atualizar legenda');
+    }
+    
+    return await response.json();
+  }
+);
 
 const uploadImagesSlice = createSlice({
   name: 'uploadImages',
@@ -117,21 +133,21 @@ const uploadImagesSlice = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder
-      .addCase(uploadMovieImage.pending, state => {
-        state.status = 'loading';
+      .addCase(uploadMovieImage.pending, (state) => {
+        state.uploadStatus = 'loading';
         state.error = null;
       })
       .addCase(uploadMovieImage.fulfilled, (state, action) => {
-        state.status = 'succeeded';
+        state.uploadStatus = 'succeeded';
         state.objectName = action.payload.object_name;
         console.log(
           'Nome do objeto (imagem) no slice:',
           action.payload.object_name
         );
       })
-      .addCase(uploadMovieImage.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload as string;
+      .addCase(uploadMovieImage.rejected, (state) => {
+        state.uploadStatus = 'failed';
+        state.error = 'An unexpected error occurred';
       })
       .addCase(fetchAllImageUrls.pending, state => {
         state.status = 'loading';
@@ -141,23 +157,33 @@ const uploadImagesSlice = createSlice({
         state.status = 'succeeded';
         state.imageUrls = action.payload.urls;
         state.imageNames = action.payload.names;
+        state.imageSubtitles = action.payload.subtitles;
       })
       .addCase(fetchAllImageUrls.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.payload as string;
       })
-      // .addCase(deleteImage.pending, state => {
-      //   state.status = 'loading';
-      //   state.error = null;
-      // })
-      // .addCase(deleteImage.fulfilled, state => {
-      //   state.status = 'succeeded';
-      //   state.error = null;
-      // })
-      // .addCase(deleteImage.rejected, (state, action) => {
-      //   state.status = 'failed';
-      //   state.error = action.payload as string;
-      // });
+      .addCase(deleteImage.pending, state => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(deleteImage.fulfilled, state => {
+        state.status = 'succeeded';
+        state.error = null;
+      })
+      .addCase(deleteImage.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload as string;
+      })
+      .addCase(updateImageSubtitle.pending, (state) => {
+        state.subtitleStatus = 'loading';
+      })
+      .addCase(updateImageSubtitle.fulfilled, (state) => {
+        state.subtitleStatus = 'succeeded';
+      })
+      .addCase(updateImageSubtitle.rejected, (state) => {
+        state.subtitleStatus = 'failed';
+      });
   },
 });
 
